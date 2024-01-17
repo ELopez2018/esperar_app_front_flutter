3a82d0275c70abecb2c92a2c69fafa2f76c6c199
clavijo99 <bclavijogomez@gmail.com>
2024-01-16 22:40:23 -0500

    created splash screen

---
+++ lib/core/routes/routes.dart
@@ -0,0 +1,7 @@
+import 'package:esperar_app_front_flutter/ux/screens.dart';
+import 'package:flutter/material.dart';
+
+routes(BuildContext context) => {
+  'splash': (context) => const SplashScreen(),
+  'welcome': (context) => const WelcomeScreen(),
+};
---
+++ lib/ux/screens.dart
@@ -0,0 +1,2 @@
+export 'package:esperar_app_front_flutter/ux/splash/splash_screen.dart';
+export 'package:esperar_app_front_flutter/ux/welcome/welcome_screen.dart';
