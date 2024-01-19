c84d5444b366320b0994bc99e3ec7447f28b9e13
clavijo99 <bclavijogomez@gmail.com>
2024-01-19 07:45:31 -0500

    news screen created

--- lib/ux/news/news_screen.dart
+++ lib/ux/news/news_screen.dart
@@ -1,3 +1,4 @@
+import 'package:esperar_app_front_flutter/ux/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class NewsScreen extends StatelessWidget {
@@ -5,8 +6,139 @@ class NewsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        children: [
+          const AppBarCustom(label: 'NOTICIAS'),
+          Padding(
+            padding: const EdgeInsets.only(top: 20, bottom: 40),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                  ),
+                  child: const Icon(Icons.add),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Container(
+                        alignment: Alignment.centerLeft,
+                        height: 40,
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black)),
+                        padding: const EdgeInsets.only(top: 15, left: 10),
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                              hintText: 'Escribe aquí si quieres compartir algo',
+                              border: InputBorder.none),
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Container(
+                        width: 90,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black)),
+                        child: const Text('COMPARTIR'),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+              child: ListView.builder(
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: EdgeInsets.only(
+                    bottom: index != 9 ? 10 : 0),
+                child: SizedBox(
+                  height: 200,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 90,
+                        height: 90,
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                          color: Colors.black,
+                        )),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 20),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                'mi empresa',
+                                style: TextStyle(fontWeight: FontWeight.w600),
+                              ),
+                              const Text(
+                                '17-08-2020 08:45 pm',
+                                style: TextStyle(fontSize: 12),
+                              ),
+                              const SizedBox(height: 5),
+                              const Expanded(
+                                  child: Text(
+                                'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos espécimen. No sólo sobrevivió 500 años, sino que también ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenían pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum',
+                                maxLines: 6,
+                                overflow: TextOverflow.ellipsis,
+                              )),
+                              const SizedBox(height: 5),
+                              Align(
+                                alignment: Alignment.bottomRight,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: [
+                                    Container(
+                                      width: 90,
+                                      alignment: Alignment.center,
+                                      decoration: BoxDecoration(
+                                          border:
+                                              Border.all(color: Colors.black)),
+                                      child: const Text('ME GUSTA'),
+                                    ),
+                                    const SizedBox(width: 10),
+                                    Container(
+                                      width: 90,
+                                      alignment: Alignment.center,
+                                      decoration: BoxDecoration(
+                                          border:
+                                              Border.all(color: Colors.black)),
+                                      child: const Text('COMENTAR'),
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            },
+            itemCount: 10,
+          ))
+        ],
+      ),
     );
   }
 }
