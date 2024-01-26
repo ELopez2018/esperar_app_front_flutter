6785e8c610993606ebd97d4190e8bd0aa883d807
clavijo99 <bclavijogomez@gmail.com>
2024-01-26 11:34:18 -0500

    create vehicle service

--- lib/data/models/vehicles/vehicle_model.dart
+++ lib/data/models/vehicles/vehicle_model.dart
@@ -1,23 +1,23 @@
 import 'package:esperar_app_front_flutter/data/models/users/user_model.dart';
 import 'package:json_annotation/json_annotation.dart';
 
-
 part 'vehicle_model.g.dart';
 
 @JsonSerializable()
 class VehicleModel {
-  VehicleModel(
-      {required this.id,
-      required this.licensePlate,
-      required this.model,
-      required this.brand,
-      required this.year,
-      required this.color,
-      required this.cylinderCapacity,
-      required this.capacity,
-      required this.occupancy,
-      required this.owner,
-      required this.driver});
+  VehicleModel({
+    required this.id,
+    required this.licensePlate,
+    required this.model,
+    required this.brand,
+    required this.year,
+    required this.color,
+    required this.cylinderCapacity,
+    required this.capacity,
+    required this.occupancy,
+    required this.owner,
+    required this.driver,
+  });
 
   final int id;
   final String licensePlate;
@@ -30,7 +30,8 @@ class VehicleModel {
   final int occupancy;
   final UserModel owner;
   final UserModel driver;
-    factory VehicleModel.fromJson(Map<String, dynamic> json) =>
+
+  factory VehicleModel.fromJson(Map<String, dynamic> json) =>
       _$VehicleModelFromJson(json);
   Map<String, dynamic> toJson() => _$VehicleModelToJson(this);
 }
--- lib/data/models/vehicles/vehicles_response_model.dart
+++ lib/data/models/vehicles/vehicles_response_model.dart
@@ -6,9 +6,14 @@ part 'vehicles_response_model.g.dart';
 
 @JsonSerializable()
 class VehiclesResponseModel {
-  VehiclesResponseModel({required this.content, required this.pageable});
+  VehiclesResponseModel({
+    required this.content,
+    required this.pageable,
+  });
+
   final List<VehicleModel> content;
   final PageableModel pageable;
+
   factory VehiclesResponseModel.fromJson(Map<String, dynamic> json) =>
       _$VehiclesResponseModelFromJson(json);
   Map<String, dynamic> toJson() => _$VehiclesResponseModelToJson(this);
