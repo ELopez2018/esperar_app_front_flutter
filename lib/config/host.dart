b3613efe3531f62e76c163c9f0abf2563dff0437
clavijo99 <bclavijogomez@gmail.com>
2024-01-26 11:41:35 -0500

    api host

--- lib/config/host.dart
+++ lib/config/host.dart
@@ -1,9 +1,7 @@
 String get apiHost {
   bool idProd = const bool.fromEnvironment("dart.vm.product");
   if (idProd) {
-    
     return "https://";
   }
-
   return "http://192.168.0.122:8080/api/v1";
 }
